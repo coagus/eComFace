99710d6234e932bf110ef12492bd55b83f7263d0
Christian Agustin <networker@christianagustin.com>
2018-03-09 07:59:20 -0600

    se agrega la pagina de login con solo un boton para ingresar

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { LoginPage } from '../login/login';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,9 +13,12 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  //tab3Root = ContactPage;
 
-  constructor() {
+  constructor(public navCtrl: NavController) {
+  }
 
+  logOut() {
+    this.navCtrl.getRootNav().setRoot(LoginPage);
   }
 }
